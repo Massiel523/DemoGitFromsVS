19dc3ef58f030d67c3dfd6661e99b25cff532593
Massiel523 <lopezmassiel523@gmail.com>
2018-09-04 10:53:27 -0600

    login forms

--- DemoGitFromVS/LoginForms.Designer.cs
+++ DemoGitFromVS/LoginForms.Designer.cs
@@ -38,33 +38,36 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(35, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.Size = new System.Drawing.Size(67, 18);
             this.label1.TabIndex = 0;
             this.label1.Text = "Usuario";
             this.label1.UseMnemonic = false;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 25);
+            this.textBox1.Location = new System.Drawing.Point(12, 30);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(260, 20);
             this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(35, 69);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.Size = new System.Drawing.Size(95, 18);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Contrasena";
+            this.label2.Text = "Contraseña";
             this.label2.UseMnemonic = false;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(12, 101);
+            this.textBox2.Location = new System.Drawing.Point(12, 90);
             this.textBox2.Name = "textBox2";
             this.textBox2.PasswordChar = '*';
             this.textBox2.ReadOnly = true;
--- DemoGitFromVS/LoginForms.cs
+++ DemoGitFromVS/LoginForms.cs
@@ -16,5 +16,10 @@ namespace DemoGitFromVS
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
